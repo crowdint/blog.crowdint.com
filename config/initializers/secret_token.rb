124e9ad16ad20b76b7b9f11a314ca6b370fd337b
David Padilla <david@crowdint.com>
2012-02-15 15:32:58 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdblog::Application.config.secret_token = '1171ab818cd71c430a8b321a139bdb3d64d69c06de84d5908b7ad2cc5e6d01fa25964af142f06cecfe0748b63de2bf73cc69acc58be7db9fa6f18e270d6062e5'
