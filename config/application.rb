ba12bfd560b4dcd7f298d09f2cb5edfafe339815
David Padilla <david@crowdint.com>
2012-02-16 10:29:11 -0600

    Prepare for admin styles

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module Crowdblog
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile += ['admin.css']
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
