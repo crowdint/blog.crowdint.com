da2e5e36bfe8b91c4d0b90317f63791267681794
David Padilla <david@crowdint.com>
2012-02-19 16:16:59 -0600

    This is the good one

--- app/sweepers/post_sweeper.rb
+++ app/sweepers/post_sweeper.rb
@@ -2,20 +2,21 @@ class PostSweeper < ActionController::Caching::Sweeper
   observe Post
 
   def after_create(post)
-    expire_all
+    expire_all(post)
   end
 
   def after_update(post)
-    expire_all
+    expire_all(post)
   end
 
   def after_destroy(post)
-    expire_all
+    expire_all(post)
   end
 
   private
-  def expire_all
+  def expire_all(post)
     expire_page(controller: '/posts', action: 'index')
-    expire_page(controller: '/posts', action: 'show')
+    expire_page(controller: '/posts', action: 'show',
+                year: post.year, month: post.month, day: post.day, id: post.permalink, format: 'html')
   end
 end
