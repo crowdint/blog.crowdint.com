5daaea497427473ca833693038fbb9f0e22dd7ce
David Padilla <david@crowdint.com>
2012-03-01 11:24:51 -0600

    Posts by author styling

--- app/controllers/posts_by_author_controller.rb
+++ app/controllers/posts_by_author_controller.rb
@@ -1,10 +1,11 @@
 class PostsByAuthorController < ApplicationController
   respond_to :html
-  
+
   def index
     @author = User.where(['email like ?', params[:author] + '@%']).first
     raise ActionController::RoutingError.new('Author Not Found') unless @author
     @posts = Post.by_author(@author.id)
+    @history = Post.for_history
     respond_with @posts
   end
 end
