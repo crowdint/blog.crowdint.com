f6ebe5dd839752a464368fef84ad27630ded4680
David Padilla <david@crowdint.com>
2012-05-03 18:52:14 -0500

    Of course! we also need crowdblog.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Crowdblog::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( crowdblog.css )
+  config.assets.precompile += %w( crowdblog.css crowdblog.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
