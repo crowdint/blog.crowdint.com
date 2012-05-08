daabd9928b9de2e5211c2e0cdd82f2bbcdaa06ca
David Padilla <david@crowdint.com>
2012-05-08 00:31:58 -0500

    Fix the archive

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < Crowdblog::Post
+  def self.grouped_for_archive
+    published_and_ordered.group_by {|p| p.published_at.year }.
+        inject({}) { |mem, value| mem[value[0]] = value[1].
+            group_by {|p| p.published_at.strftime("%B")}; mem }
+  end
 end
