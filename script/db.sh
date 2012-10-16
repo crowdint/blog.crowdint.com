d85cad5579f5440256d0217232cbebbd6125e6a7
David Padilla <david@crowdint.com>
2012-10-16 00:26:06 -0500

    Modified the DB script for Heroku toolbelt

--- script/db.sh
+++ script/db.sh
@@ -5,10 +5,10 @@ TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 FILENAME=$TIMESTAMP.sql
 
 echo "Capture the database"
-bundle exec heroku pgbackups:capture
+heroku pgbackups:capture
 
 echo "Download the backup"
-wget -O $FILENAME `bundle exec heroku pgbackups:url`
+wget -O $FILENAME `heroku pgbackups:url`
 
 echo "Restore the database"
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d $DATABASE $FILENAME
