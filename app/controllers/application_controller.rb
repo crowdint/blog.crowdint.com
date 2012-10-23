891358f3d9d7ce7478cca42968b67a899de47803
David Padilla <david@crowdint.com>
2012-10-22 20:32:15 -0500

    Fix admin after log in redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    crowdblog_url
+    crowdblog.admin_root_url
   end
 
   def after_sign_out_path_for(resource)
