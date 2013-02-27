a01653b2a74f6d931d15c514927a236b72f67ac5
David Padilla <david@crowdint.com>
2013-02-27 13:38:04 -0800

    Search only published posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < Crowdblog::Post
   belongs_to :author, :class_name => "User"
   belongs_to :publisher, :class_name => "User"
 
+  searchable do
+    text :title, :body
+    string :state
+  end
+
   def self.grouped_for_archive
     published_and_ordered.group_by {|p| p.published_at.year }.
         inject({}) { |mem, value| mem[value[0]] = value[1].
@@ -16,13 +21,10 @@ class Post < Crowdblog::Post
     body.first(300).gsub(/\n/, '') + "..."
   end
 
-  searchable do
-    text :title, :body
-  end
-
   def self.query(query)
     Post.search do
       fulltext query
+      with :state, 'published'
     end
   end
 end
