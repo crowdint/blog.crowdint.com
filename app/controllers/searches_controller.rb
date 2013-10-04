44deef7ff92b3e70f0d6774ee6ca7bab3c67789d
David Padilla <david@crowdint.com>
2013-10-04 15:20:24 +0200

    Search page now ready for the phone view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    return redirect_to root_path unless params[:q].present?
-
     @query = params[:q]
     @posts = Post.query(@query).results
   end
