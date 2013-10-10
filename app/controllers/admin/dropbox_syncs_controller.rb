046c7b2eccb75eafb156797b43d73c0ca6215d37
David Padilla <david@crowdint.com>
2013-10-10 16:25:48 +0300

    Disable Dropbox Sync because I'm not sure how it works

--- app/controllers/admin/dropbox_syncs_controller.rb
+++ app/controllers/admin/dropbox_syncs_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DropboxSyncsController < Crowdblog::ApplicationController
   def new
-    current_user.user_dropbox_session.sync
+    #current_user.user_dropbox_session.sync
     redirect_to crowdblog.posts_path
   end
 end
