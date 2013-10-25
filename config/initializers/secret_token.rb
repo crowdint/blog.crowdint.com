157323a8e45404e26010f1f46e10badf43ad28ca
David Padilla <david@crowdint.com>
2013-10-25 13:17:09 -0500

    Remove the session secret and read it from ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Crowdblog::Application.config.secret_token = '1171ab818cd71c430a8b321a139bdb3d64d69c06de84d5908b7ad2cc5e6d01fa25964af142f06cecfe0748b63de2bf73cc69acc58be7db9fa6f18e270d6062e5'
+Crowdblog::Application.config.secret_token = ENV['SESSION_SECRET']
