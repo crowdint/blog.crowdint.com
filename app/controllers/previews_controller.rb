9025c80c7f76254b884dda3d4f1a7e16dcffa6be
Mario A Chavez <mario.chavez@gmail.com>
2014-09-10 12:29:32 -0500

    Get post by author for preview

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -2,6 +2,8 @@ class PreviewsController < ApplicationController
   def show
     @post = Post.includes(:author).find(params[:id])
     @post.published_at = Date.today
+
+    @post_by_author = Crowdblog::Post.where(state: 'published').where('id <> ?', @post).where(author_id: @post.author).order('published_at DESC, id DESC').limit(3)
     render 'crowdblog/posts/show'
   end
 end
