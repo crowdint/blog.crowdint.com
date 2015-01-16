234feb96b674d1b4a9cbc4d81a10b927076a6208
Jaime Victoria <jaime.victoria@crowdint.com>
2015-01-16 16:58:48 -0600

    SEO, set meta data before rendering

--- app/controllers/posts_by_author_controller.rb
+++ app/controllers/posts_by_author_controller.rb
@@ -6,6 +6,7 @@ class PostsByAuthorController < ApplicationController
     raise ActionController::RoutingError.new('Author Not Found') unless @author
     @posts = Post.by_author(@author.id)
     @history = Post.for_history
+    set_meta
     respond_with @posts
   end
 end
