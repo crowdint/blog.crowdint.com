099d8eec1974bb507743e08c103f5a7b5677ce48
Jaime Victoria <jaime.victoria@crowdint.com>
2015-01-19 16:32:26 -0600

    [#85450732] create categories form admin and add categories to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < Crowdblog::Post
   belongs_to :author, :class_name => "User"
   belongs_to :publisher, :class_name => "User"
-  belongs_to :category
+  has_and_belongs_to_many :categories
 
   SHORT_DESCRIPTION_SIZE = 300
 
