4470c0c7d526c61d4ef67b1a878add8fac0be8e2
Juan Carlos <zalapeach@gmail.com>
2015-01-29 10:58:23 -0600

    creates seeds for categories and add a default category for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+['eCommerce',
+ 'Open Source',
+ 'Mobile Dev',
+ 'Spree',
+ 'UX/UI Design',
+ 'Community',
+ 'Javascript',
+ 'iOS'].each do |category|
+   Category.find_or_create_by(name: category)
+ end
+
+Post.all.each do |post|
+  post.categories = []
+  post.categories << Category.find_by(name: 'Community')
+end
