61ab5976b6d6438d50c55c1f2d3bce0e19aef13f
Juan Carlos <zalapeach@gmail.com>
2015-02-02 14:12:54 -0600

    move dash to previous line

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -2,8 +2,7 @@ pages.categories.mobileCategories = function(){
   $('.categories-select').on('change', function(){
     var category = $(this).find('option:selected').text().toLowerCase();
     var url = '/categories/' +
-      $(this).val() +
-      '-' +
+      $(this).val() + '-' +
       category.split(' ').join('-');
     if (url) {
       window.location.replace(url);
