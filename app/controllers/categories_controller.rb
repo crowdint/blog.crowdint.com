0975abf26ea04621ab84a5ab045299d3787ce7ab
Juan Carlos <zalapeach@gmail.com>
2015-02-02 14:51:16 -0600

    Make categories menu work

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    @categories = Category.with_posts
     @posts = @category.published_posts.
       paginate(page: params[:page], per_page: 4)
   end
