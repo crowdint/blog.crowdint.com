632f8d32f4ce92e04e009a14704c076a1a356eeb
Juan Carlos <zalapeach@gmail.com>
2015-02-20 16:39:15 -0600

    [#87323012][1pts] Search box
    * Fix specs
    
    Search box [Finishes #87323012]

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,22 +7,4 @@ describe ApplicationHelper do
         to eq "<a class=\"yolo\" href=\"#\">ola k ace</a>"
     end
   end
-
-  describe '#index_page?' do
-    context 'index page' do
-      it 'returns index' do
-        params = { controller: 'crowdblog/posts', action: 'index' }
-        allow(helper).to receive(:params).and_return(params)
-        expect(helper.index_page?).to eq 'index'
-      end
-    end
-
-    context 'another page' do
-      it 'returns nil' do
-        params = { controller: 'crowdblog/posts', action: 'show' }
-        allow(helper).to receive(:params).and_return(params)
-        expect(helper.index_page?).to eq nil
-      end
-    end
-  end
 end
