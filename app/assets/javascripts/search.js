6572cfa2f6910d01ecec4f92ef1413d5100e6f57
Juan Carlos Aguilar Rincón <juan.aguilar@crowdint.com>
2015-02-24 16:29:29 -0600

    [#87323012][1pts] Search box
    * Fix bug for search in destop
    * Fix style comments
    
    Search box [Finishes #87323012]

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,13 +1,20 @@
+var tabletSize = 1024;
+var escapeKey = 27;
+
 pages.search.showForm = function(){
-  $('.js-toggle-search').click(function(){
-    $('.js-search').css({ 'display': 'block', 'margin-top': '-75px' });
-  });
+  if ($(document).width() < tabletSize) {
+    $('.js-toggle-search').click(function(){
+      $('.js-search').css({ 'display': 'block', 'margin-top': '-75px' });
+    });
+  }
 };
 
 pages.search.hideForm = function(){
-  $('body').keyup(function(event){
-    if (event.keyCode === 27){
-      $('.js-search').css({ 'display': 'none', 'margin-top': '0' });
-    }
-  });
+  if ($(document).width() < tabletSize) {
+    $('body').keyup(function(event){
+      if (event.keyCode === escapeKey){
+        $('.js-search').css({ 'display': 'none', 'margin-top': '0' });
+      }
+    });
+  }
 }
