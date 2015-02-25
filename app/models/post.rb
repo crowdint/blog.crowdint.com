705744a659f0194a57da48e545f3505ac4611411
Juan Carlos Aguilar Rincón <juan.aguilar@crowdint.com>
2015-02-25 16:19:27 -0600

    [#88452762][5pts] Archived posts
    * Adds styles for archive in desktop
    * Creates social media mixin
    * Edit archive view and controller
    * Creates posts css module
    * Edit archived posts in the model
    
    Archived posts [Finishes #88452762]

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,9 +13,7 @@ class Post < Crowdblog::Post
   end
 
   def self.grouped_for_archive
-    published_and_ordered.group_by {|p| p.published_at.year }.
-        inject({}) { |mem, value| mem[value[0]] = value[1].
-            group_by {|p| p.published_at.strftime("%B")}; mem }
+    published_and_ordered.group_by { |p| p.published_at.year }
   end
 
   def category_names
